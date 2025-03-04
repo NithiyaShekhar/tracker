32696c0d7d7b1a0c138ca3762a0651e496966dfa
nithiyagunashekhar <nithiya.g@thewholewave.com>
2025-03-04 09:51:55 +0530

    API URL changed

--- tracker.js
+++ tracker.js
@@ -1,5 +1,5 @@
   (function () {
-    const API_ENDPOINT = "https://98ae-60-243-64-58.ngrok-free.app/webhook";
+    const API_ENDPOINT = "https://b6cf-60-243-64-58.ngrok-free.app/webhook";
 
     let sessionStartTime = Date.now();
 
